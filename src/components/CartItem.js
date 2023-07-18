de304319a02d5559aad797f89424c85450f32283
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 00:27:32 -0400

    Add the actions of the cart reducer in the products buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,43 +1,58 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { ChevronDown, ChevronUp } from '../icons';
 
-const CartItem = ({
-  id, img, title, price, amount,
-}) => (
-  <article className="cart-item">
-    <img src={img} alt={title} />
-    <div>
-      <h4>{title}</h4>
-      <h4 className="item-price">
-        $
-        {price}
-      </h4>
-      <button
-        type="button"
-        className="remove-btn"
-      >
-        remove
-        {' '}
-        {id}
-      </button>
-    </div>
-    <div>
-      <button
-        type="button"
-        className="amount-btn"
-      >
-        <ChevronUp />
-      </button>
-      <p className="amount">{amount}</p>
-      <button
-        type="button"
-        className="amount-btn"
-      >
-        <ChevronDown />
-      </button>
-    </div>
-  </article>
-);
+const CartItem = ({ id, img, title, price, amount, }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">
+          $
+          {price}
+        </h4>
+        <button
+          type="button"
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button
+          type="button"
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
 
 CartItem.propTypes = {
   id: PropTypes.string.isRequired,
@@ -46,4 +61,5 @@ CartItem.propTypes = {
   price: PropTypes.number.isRequired,
   amount: PropTypes.number.isRequired,
 };
+
 export default CartItem;
