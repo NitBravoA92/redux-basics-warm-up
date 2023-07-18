dbcef35cac90655500db859d79e2c83eca0de94b
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 12:12:42 -0400

    Update up and down arrows icons

--- src/icons.js
+++ src/icons.js
@@ -18,25 +18,29 @@ export const CartIcon = () => (
 export const ChevronDown = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
+    fill="none"
   >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    <path
+      d="M12.0002 16.4141L19.4144 8.99991L18.0002 7.58569L12.0002 13.5857L6.00015 7.58569L4.58594 8.99991L12.0002 16.4141Z"
+      fill="black"
+    />
   </svg>
 );
 
 export const ChevronUp = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
+    fill="none"
   >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
+    <path
+      d="M4.58594 14.9999L12.0002 7.58569L19.4144 14.9999L18.0002 16.4141L12.0002 10.4141L6.00015 16.4141L4.58594 14.9999Z"
+      fill="black"
+    />
   </svg>
 );
