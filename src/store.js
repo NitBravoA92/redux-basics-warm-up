966bf4f8eb56a514df4ee2aec3e89736a5edfdde
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 00:47:10 -0400

    Import the modalReducer from modalSlice

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import modalReducer from './features/modal/modalSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
 
