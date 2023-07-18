08338822d04b6e59bb3abd1d2af7ef50ad0fa660
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 00:19:48 -0400

    Create the actions removeItem, increase, decrease in the cart reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -16,8 +16,20 @@ const cartSlice = createSlice({
       state.cartItems = [];
       state.amount = 0; 
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
