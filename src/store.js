0c08125f98653639dd4171ebec3504b94a3f4efd
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 21:26:37 -0400

    Import the cart reducer from cartSlice

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
 
-const store = ({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
 
 export default store;
