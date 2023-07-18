c1790c7d207f7b6500b2fc84d26f7d16532b30d5
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 11:58:16 -0400

    Load the API call in useEffect hook to get the Products data and update the state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import Modal from './components/Modal';
 import './App.css';
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading products...</h1>
+      </div>
+    );
+  }
 
   return (
     <main className="App">
