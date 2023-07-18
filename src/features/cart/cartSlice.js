cb32c80bdcc4db31abbb1eaa7a76c3ecd034116b
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 00:32:50 -0400

    Create the calculateTotals action

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -28,6 +28,16 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount - 1;
     },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
