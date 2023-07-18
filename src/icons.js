9dca1b1f5a59f1cc6f27e9adf845ce7f398ec893
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 22:51:57 -0400

    Create the CartItem component

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,4 @@
-const CartIcon = () => {
+export const CartIcon = () => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -17,4 +17,32 @@ const CartIcon = () => {
   );
 };
 
-export default CartIcon;
+export const ChevronDown = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+    </svg>
+  );
+};
+
+export const ChevronUp = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+    </svg>
+  );
+};
